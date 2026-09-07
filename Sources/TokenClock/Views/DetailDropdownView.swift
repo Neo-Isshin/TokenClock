30991a99905b09947caf7ae50c9d178ee331b71b
nxc8335 <nxc8335@gmail.com>
2026-09-07 00:14:22 -0700

    Finish forecast time alignment

--- Sources/TokenClock/Views/DetailDropdownView.swift
+++ Sources/TokenClock/Views/DetailDropdownView.swift
@@ -646,7 +646,7 @@ struct DetailDropdownView: View {
                     Text(L10n.shared.tr("detail.forecast"))
                         .font(.system(size: 11))
                         .foregroundColor(subtextColor)
-                        .padding(.trailing, 3)
+                        .offset(x: 2)
                 }
                 forecastNotificationButton
             }
