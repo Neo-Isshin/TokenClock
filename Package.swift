17f3fa232a25247133e5010f8fdf7a38000e9459
nxc8335 <nxc8335@gmail.com>
2026-09-07 06:30:47 -0700

    Revert "Add custom provider and model icons"
    This reverts commit de733542d41b3e886af36d085f76489f14134485.

--- Package.swift
+++ Package.swift
@@ -91,7 +91,6 @@ let package = Package(
             resources: [
                 .copy("Resources/glass_disc.png"),
                 .copy("Resources/pricing-snapshot.json"),
-                .copy("Resources/TokenClockIcons"),
             ],
             swiftSettings: [.unsafeFlags(["-parse-as-library"])]
         ),
@@ -147,7 +146,6 @@ let package = Package(
             resources: [
                 .copy("Resources/glass_disc.png"),
                 .copy("Resources/pricing-snapshot.json"),
-                .copy("Resources/TokenClockIcons"),
             ],
             swiftSettings: [.unsafeFlags(["-parse-as-library"])],
             // SwiftPM otherwise emits a console-subsystem PE and Windows opens a black terminal
@@ -176,7 +174,6 @@ let package = Package(
             resources: [
                 .copy("Resources/glass_disc.png"),
                 .copy("Resources/pricing-snapshot.json"),
-                .copy("Resources/TokenClockIcons"),
             ],
             swiftSettings: [.unsafeFlags(["-parse-as-library"])]
         ),
