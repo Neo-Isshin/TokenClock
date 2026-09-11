d52045baf1edb1497f0dff3281d35f95dd2dc1b9
nxc8335 <nxc8335@gmail.com>
2026-09-10 18:17:08 -0700

    release: prepare v1.5.7

--- cli/install.sh
+++ cli/install.sh
@@ -228,9 +228,9 @@ release_tag_for_variant() {
     return
   fi
   case "$1" in
-    glass)  echo "v1.5.6" ;;
-    normal) echo "v1.5.6" ;;
-    linux)  echo "v1.5.6" ;;
+    glass)  echo "v1.5.7" ;;
+    normal) echo "v1.5.7" ;;
+    linux)  echo "v1.5.7" ;;
   esac
 }
 release_base_for_tag() {
