4f46d9ff8292a4fac924afc3935461b758479a78
nxc8335 <nxc8335@gmail.com>
2026-09-26 10:36:34 -0700

    Prepare v1.5.10 release version metadata

--- Sources/TokenClock/Windows/WindowsApp.swift
+++ Sources/TokenClock/Windows/WindowsApp.swift
@@ -1140,7 +1140,7 @@ final class WindowsApp: @unchecked Sendable {
         defer { aboutDlg = nil; dlg_destroy(dlg) }
         dlg_add_brand_logo(dlg, 136, 22, 88, 88)
         brand_add_title(dlg, "TokenClock", 112, 120, 180, 30)
-        brand_add_static(dlg, "v1.5.9", 154, 154, 90, 22)
+        brand_add_static(dlg, "v1.5.10", 154, 154, 90, 22)
         dlg_add_sep(dlg, 28, 188, 304)
         brand_add_static(dlg, "Copyright © 2026 Neo-Isshin", 78, 210, 250, 22)
         brand_add_static(dlg, L10n.shared.tr("about.license"), 128, 238, 180, 22)
